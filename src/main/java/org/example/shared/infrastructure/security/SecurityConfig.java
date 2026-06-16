9b0ac8da4f3a1510fa49b60e5fe9ea713b4c4215
misarkisiani <MSarkisiani@tbcbank.com.ge>
2026-06-16 18:23:30 +0400

    Fix(auditorProvider): Prevented NullPointerException crash by introducing Optional.ofNullable

--- src/main/java/org/example/shared/infrastructure/security/SecurityConfig.java
+++ src/main/java/org/example/shared/infrastructure/security/SecurityConfig.java
@@ -2,12 +2,17 @@ package org.example.shared.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Optional;
 
 
 @Configuration
@@ -40,6 +45,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(SECURITY_STRENGTH);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .filter(auth -> auth.isAuthenticated() && !"anonymousUser".equals(auth.getPrincipal()))
+                .map(Authentication::getName);
     }
 }
 
